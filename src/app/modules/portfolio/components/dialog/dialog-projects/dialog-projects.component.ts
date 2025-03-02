4a21b41c5f8ab323bd08974075cc16d6f78cd55c
Fábio Valentim <maitabom@gmail.com>
2025-03-02 16:18:10 -0300

    Implementação da função da caixa de diálogo

--- src/app/modules/portfolio/components/dialog/dialog-projects/dialog-projects.component.ts
+++ src/app/modules/portfolio/components/dialog/dialog-projects/dialog-projects.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, signal } from '@angular/core';
+import { Project } from '../../../interfaces/projects.interface';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-dialog-projects',
-  imports: [],
-  templateUrl: './dialog-projects.component.html',
-  styleUrl: './dialog-projects.component.scss'
+    selector: 'app-dialog-projects',
+    imports: [MatDialogModule],
+    templateUrl: './dialog-projects.component.html',
+    styleUrl: './dialog-projects.component.scss',
 })
-export class DialogProjectsComponent {
+export class DialogProjectsComponent implements OnInit {
+    public getData = signal<Project | null>(null);
 
+    constructor(private _dialogRef: MatDialogRef<DialogProjectsComponent>, @Inject(MAT_DIALOG_DATA) private _data: Project) {}
+
+    ngOnInit(): void {
+        this.getData.set(this._data);
+    }
+
+    closeModal() {
+        this._dialogRef.close();
+    }
 }
